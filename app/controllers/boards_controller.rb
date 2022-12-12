cc0209f756490c4598f6921e0e261b042ac3dc41
Guesanjo <guesanjoupv@gmail.com>
2022-12-12 17:16:35 +0100

    create boards

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,21 @@
 class BoardsController < ApplicationController
     before_action :authenticate_user!
-    
+
     def new
         @board = Board.new
+    end
+
+    def create
+        @board = Board.new(board_params.merge(user: current_user))
+
+        if @board.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    def board_params
+        params.require(:board).permit(:name)
     end
 end
