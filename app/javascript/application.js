2147ffcbf355856ac0fd9c741db53344d5294ab8
guesanjo <guesanjoupv@gmail.com>
2022-12-31 17:13:55 +0100

    turbo mehtods arreglado

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/stimulus"
+import "@hotwired/turbo-rails"
+import "@hotwired/stimulus-loading"
 import "controllers"
 import "jkanban"
 //=require_tree .
+
