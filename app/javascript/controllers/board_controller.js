e5c49b4a0157cd24a3fdfe311beb280ff14f74a0
guesanjo <guesanjoupv@gmail.com>
2023-01-07 16:54:07 +0100

    funcional random js color

--- app/javascript/controllers/board_controller.js
+++ app/javascript/controllers/board_controller.js
@@ -1,49 +1,47 @@
 import { Controller } from "@hotwired/stimulus";
 import axios from "axios";
-import { get, map, isNull } from "lodash-es";
+import { get, map , sample } from "lodash-es";
 
 export default class extends Controller {
   HEADERS = { ACCEPT: "application/json" };
+  BACKGROUND_COLORS = ['bg-info', 'bg-primary', 'bg-secondary', 'bg-success', 'bg-warning', 'bg-danger', 'bg-dark'];
 
   connect() {
-    axios
-      .get("/api/boards/1/lists", { headers: this.HEADERS })
+    axios.get(this.element.dataset.apiUrl, { headers: this.HEADERS })
       .then((response) => {
-        this.buildKanban(this.buildBoards(response["data"]));
+        this.buildKanban(this.buildBoards(response['data']));   
       });
   }
 
-  buildClassList(classList) {
-    if (isNull(classList)) {
-      return "";
-    }
-    return classList.split(" ").join(", ");
+
+  buildClassList(){
+    return `text-white, ${sample(this.BACKGROUND_COLORS)}`;
   }
+
 
   buildItems(items) {
     return map(items, (item) => {
       return {
-        id: get(item, "id"),
-        title: get(item, "attributes.title"),
-        class: this.buildClassList(get(item, "attributes.class_list")),
+        'id': get(item, 'id'),
+        'title': get(item, 'attributes.title'),
       };
     });
   }
 
   buildBoards(boardsData) {
-    return map(boardsData["data"], (board) => {
+    return map(boardsData['data'], (board) => {
       return {
-        id: get(board, "id"),
-        title: get(board, "attributes.title"),
-        class: this.buildClassList(get(board, "attributes.class_list")),
-        item: this.buildItems(get(board, "attributes.items.data")),
+        'id': get(board, 'id'),
+        'title': get(board, 'attributes.title'),
+        'class': this.buildClassList(),
+        'item': this.buildItems(get(board, 'attributes.items.data')),
       };
     });
   }
 
   buildKanban(boards) {
     new jKanban({
-      element: "#board", // selector of the kanban container
+      element: `#${this.element.id}`, // selector of the kanban container
       boards: boards, // json of boards
       itemAddOptions: {
         enabled: true, // add a button to board for easy item creation                                     // position the button on footer
