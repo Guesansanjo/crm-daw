557ec8c9b891135b5b3d3b7fbddadb2c24e1a5d9
guesanjo <guesanjoupv@gmail.com>
2023-01-07 15:18:26 +0100

    reformat js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/stimulus"
-import "@hotwired/turbo-rails"
-import "@hotwired/stimulus-loading"
-import "controllers"
-import "jkanban"
-import "axios"
+import "@hotwired/stimulus";
+import "@hotwired/turbo-rails";
+import "@hotwired/stimulus-loading";
+import "controllers";
+import "jkanban";
+import "axios";
 //=require_tree .
-
